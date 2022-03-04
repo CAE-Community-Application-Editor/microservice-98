957902e4f36d72d63a6c8c106e856609bcc3622b
CAEUser <mail@rwth>
2022-03-04 08:46:16 +0000

    Test2

--- src/main/i5/las2peer/services/res/Res.java
+++ src/main/i5/las2peer/services/res/Res.java
@@ -107,8 +107,7 @@ public class Res extends RESTService {
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.TEXT_PLAIN)
   @ApiResponses(value = {
-       @ApiResponse(code = HttpURLConnection.HTTP_BAD_REQUEST, message = "br_name"),
-       @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "resp")
+       @ApiResponse(code = HttpURLConnection.HTTP_BAD_REQUEST, message = "br_name")
   })
   @ApiOperation(value = "getSongs", notes = " ")
   public Response getSongs(String pl) {
